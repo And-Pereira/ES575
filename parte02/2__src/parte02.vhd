332b425adc9d0474b5b6166433b57118af83cf5a
Andre-WPP <andre.william.a@hotmail.com>
2025-03-29 01:48:40 -0300

    All tests have passed!

--- parte02/2__src/parte02.vhd
+++ parte02/2__src/parte02.vhd
@@ -17,7 +17,7 @@ END ENTITY PARTE02;
 
 ARCHITECTURE ARCH_PARTE02 OF PARTE02 IS
     SIGNAL Z: std_logic;
-    SIGNAL Y_CA: Std_logic_vector(3 DOWNTO 0);
+    SIGNAL Y_CA: Std_logic_vector(2 DOWNTO 0);
     SIGNAL Y_CB: Std_logic_vector(3 DOWNTO 0);
     SIGNAL Y_MUX: Std_logic_vector(3 DOWNTO 0);
 
@@ -25,10 +25,10 @@ BEGIN
 	-- Porting all components
     COMP: COMPARATOR_NBIT
     GENERIC MAP (4)
-    PORT MAP (SW, "1010", Z);
+    PORT MAP (SW, "1001", Z);
 
     CA: CIRCUITA
-    PORT MAP (SW(2 DOWNTO 0), Y_CA(2 DOWNTO 0));
+    PORT MAP (SW(2 DOWNTO 0), Y_CA);
 
     Y_MUX(3) <= SW(3) WHEN Z = '0' ELSE '0';
     Y_MUX(2) <= SW(2) WHEN Z = '0' ELSE Y_CA(2);
@@ -37,14 +37,12 @@ BEGIN
 
     CB: CIRCUITB
     PORT MAP (Z, Y_CB);
-
-		  
+	  
     HEX1_CONV_4_TO_7: CONV_4_TO_7
     PORT MAP (Y_CB, HEX1);
         
-    Y_CA(3) <= '0';
     HEX0_CONV_4_TO_7: CONV_4_TO_7
-    PORT MAP (Y_CA, HEX0);
+    PORT MAP (Y_MUX, HEX0);
 
 
 
