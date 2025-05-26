8cdc85ce16c63cb165b28ec87785afbf563c7924
Andre-WPP <andre.william.a@hotmail.com>
2025-05-25 21:14:05 -0300

    Functional part is ready!

--- parte01/2__src/parte01.vhd
+++ parte01/2__src/parte01.vhd
@@ -7,115 +7,80 @@ USE ATOMIC_COMPONENTS.ATOMIC_COMPONENTS.ALL;
 
 ENTITY PARTE01 IS
 	PORT(
-    RST: IN STD_LOGIC;
-    CLOCK_50: IN STD_LOGIC;
-    Y_P_Vd, Y_P_Am, Y_P_Vm: OUT STD_LOGIC;
-    Y_S_Vd, Y_S_Am, Y_S_Vm: OUT STD_LOGIC;
-    Vs: IN STD_LOGIC
+    KEY: IN STD_LOGIC_VECTOR(1 DOWNTO 0); -- CLKR, ClearRegs
+    SW: IN STD_LOGIC_VECTOR(12 DOWNTO 0); -- TecD, TecE, WrEn, SelM2, SelM1, SelRB, SelRA, AddSubR
+    -- HEX0: OUT STD_LOGIC_VECTOR(6 DOWNTO 0); -- Data
+    -- HEX1: OUT STD_LOGIC_VECTOR(6 DOWNTO 0); -- Data
+    -- HEX6: OUT STD_LOGIC_VECTOR(6 DOWNTO 0); -- Address
+    -- HEX7: OUT STD_LOGIC_VECTOR(6 DOWNTO 0); -- Address
+    LEDG: OUT STD_LOGIC -- WrEn
+
 	);
 	
 END ENTITY PARTE01;
 
 ARCHITECTURE ARCH_PARTE01 OF PARTE01 IS
 
-	TYPE STATE_TYPES IS (Vd_Vm, Am_Vm, Vm_Vd, Vm_Am);
-	SIGNAL Y_Q, Y_D: STATE_TYPES; -- Y_Q: FUTURE, Y_D: CURRENT
-  SIGNAL Tmin, Tmax, Tam: std_logic;
-  SIGNAL EN_Tam, EN_Tmin, EN_Tmax: std_logic;
+  SIGNAL CLKR, ClearRegs: STD_LOGIC;
+  SIGNAL WrEn: STD_LOGIC_VECTOR(0 DOWNTO 0);
+  SIGNAL TecD: STD_LOGIC_VECTOR(7 DOWNTO 0);
+  SIGNAL TecE: STD_LOGIC_VECTOR(3 DOWNTO 0);
+
+  SIGNAL ADD: STD_LOGIC_VECTOR(3 DOWNTO 0);
+  SIGNAL DATA_IN: STD_LOGIC_VECTOR(7 DOWNTO 0);
+  SIGNAL DATA_OUT: STD_LOGIC_VECTOR(7 DOWNTO 0);
+  SIGNAL WR: STD_LOGIC_VECTOR (0 DOWNTO 0);
 
 BEGIN
+  CLKR <= KEY(0);
+  ClearRegs <= KEY(1);
 
-	-- State's table
-	PROCESS(Tmin, Tmax, Tam, Vs, Y_D)
-	BEGIN
-		CASE Y_D IS
-			-- Case 1
-			WHEN Vd_Vm=>
-				IF (Tmin = '1' and Vs = '1') THEN Y_Q <= Am_Vm;
-				ELSE Y_Q <= Vd_Vm;
-				END IF;
+  TecD <= SW(7 DOWNTO 0);
+  TecE <= SW(11 DOWNTO 8);
 
-			-- Case 2
-			WHEN Am_Vm=>
-				IF (Tam = '1') THEN Y_Q <= Vm_Vd;
-				ELSE Y_Q <= Am_Vm;
-				END IF;
+  WrEn(0) <= SW(12);
 
-      -- Case 3
-			WHEN Vm_Vd=>
-				IF (Tmax = '1' or Vs = '0') THEN Y_Q <= Vm_Am;
-				ELSE Y_Q <= Vm_Vd;
-				END IF;
-
-      -- Case 4
-			WHEN Vm_Am=>
-				IF (Tam = '1') THEN Y_Q <= Vd_Vm;
-				ELSE Y_Q <= Vm_Am;
-				END IF;
-
-		END CASE;
-	END PROCESS;
-
-	-- How FFs will behave
-	PROCESS(CLOCK_50, RST)
-	BEGIN
-		IF(RST = '1') THEN
-			Y_D <= Vd_Vm;
-
-		ELSIF (rising_edge(CLOCK_50)) THEN
-			Y_D <= Y_Q;
-
-		END IF;
-	END PROCESS;
-
-  -- Input definition
-
-  EN_Tam <= (Tmin = '1' and Vs = '1') OR (Tmax = '1' or Vs = '0');
-  -- For AM T = 1s
-  CONT_AM: TEMPORIZADOR_NBIT
+  FF_END: FLIP_FLOP_D
     GENERIC MAP(
-      4, 1
-
+        3
     )
     PORT MAP(
-      CLOCK_50, RST, EN_Tam, 
-      Tam
-
+        CLKR,
+        ClearRegs,
+        TecE,
+        ADD
     );
 
-  EN_Tmin <= Tam;
-  -- For AM T = 3s
-  CONT_MIN: TEMPORIZADOR_NBIT
+  FF_ENT_DADO: FLIP_FLOP_D
     GENERIC MAP(
-      4, 3
-
+        7
     )
     PORT MAP(
-      CLOCK_50, RST, EN_Tmin, 
-      Tmin
-
+        CLKR,
+        ClearRegs,
+        TecD,
+        DATA_IN
     );
 
-  EN_Tmax <= Tam;
-  -- For MAX T = 5s
-  CONT_MAX: TEMPORIZADOR_NBIT
+  FF_ESCREVE: FLIP_FLOP_D
     GENERIC MAP(
-      4, 5
-
+        0
     )
     PORT MAP(
-      CLOCK_50, RST, EN_Tmax, 
-      Tmax
-
+        CLKR,
+        ClearRegs,
+        WrEn,
+        WR
     );
-  
 
-	-- Output definition
-  Y_P_Vd <= '1' WHEN (Y_D = Vd_Vm) ELSE '0';
-  Y_P_Am <= '1' WHEN (Y_D = Am_Vm) ELSE '0';
-  Y_P_Vm <= '1' WHEN (Y_D = Vm_Vd OR Y_D = Vm_Am) ELSE '0';
-  Y_S_Vd <= '1' WHEN (Y_D = Vm_Vd) ELSE '0';
-  Y_S_Am <= '1' WHEN (Y_D = Vm_Am) ELSE '0';
-  Y_S_Vm <= '1' WHEN (Y_D = Vd_Vm OR Y_D = Am_Vm) ELSE '0';
+
+  RAM_16: RAM_32x8
+      PORT MAP(
+          ADD,
+          DATA_IN,
+          WR(0),
+          CLKR,
+          DATA_OUT
+      );
 
 END ARCHITECTURE ARCH_PARTE01;
--- parte02/2__src/parte02.vhd
+++ parte02/2__src/parte02.vhd
@@ -5,83 +5,103 @@ USE IEEE.std_logic_1164.all;
 LIBRARY ATOMIC_COMPONENTS;
 USE ATOMIC_COMPONENTS.ATOMIC_COMPONENTS.ALL;
 
-ENTITY PARTE02 IS
+ENTITY PARTE01 IS
 	PORT(
-	X: IN STD_LOGIC;
-	RST: IN STD_LOGIC;
-	CLK: IN STD_LOGIC;
-	Y: OUT STD_LOGIC  
+    KEY: IN STD_LOGIC_VECTOR(3 DOWNTO 0); -- CLKZ, CLKM, CLKR, ClearRegs
+    SW: IN STD_LOGIC_VECTOR(17 DOWNTO 0); -- TecD, TecE, WrEn, SelM2, SelM1, SelRB, SelRA, AddSubR
+    HEX: OUT STD_LOGIC_VECTOR(1 DOWNTO 0); -- Data
+    HEX: OUT STD_LOGIC_VECTOR(7 DOWNTO 6); -- Address
+    LEDG: OUT STD_LOGIC_VECTOR(0); -- WrEn
+
 	);
 	
-END ENTITY PARTE02;
+END ENTITY PARTE01;
 
-ARCHITECTURE ARCH_PARTE02 OF PARTE02 IS
+ARCHITECTURE ARCH_PARTE01 OF PARTE01 IS
 
-	TYPE STATE_TYPES IS (A, B, C, D, E, F, G, H, I);
-	SIGNAL Y_Q, Y_D: STATE_TYPES; -- Y_Q: FUTURE, Y_D: CURRENT
+	SIGNAL CLKZ, CLKM, CLKR, ClearRegs, WrEn, SelM2, SelM1, SelRB, SelRA, AddSubR: STD_LOGIC;
+  SIGNAL TecD: STD_LOGIC_VECTOR(7 DOWNTO 0);
+  SIGNAL TecE: STD_LOGIC_VECTOR(3 DOWNTO 0);
+
+  SIGNAL ADD: STD_LOGIC_VECTOR(3 DOWNTO 0);
+  SIGNAL DATA_IN: STD_LOGIC_VECTOR(7 DOWNTO 0);
+  SIGNAL DATA_OUT: STD_LOGIC_VECTOR(7 DOWNTO 0);
+  SIGNAL WR: STD_LOGIC;
 
 BEGIN
+  CLKZ <= KEY(0);
+  CLKM <= KEY(1);
+  CLKR <= KEY(2);
+
+  ClearRegs <= KEY(3);
+
+  TecD <= SW(7 DOWNTO 0);
+  TecE <= SW(11 DOWNTO 8);
+
+  WrEn <= SW(12);
+
+  SelM2 <= SW(13);
+  SelM1 <= SW(14);
+  SelRB <= SW(15);
+  SelRA <= SW(16);
+  AddSubR <= SW(17);
+
+  FF_END: FLIP_FLOP_D
+    GENERIC MAP(
+        4
+    );
+    PORT MAP(
+        CLKR,
+        ClearRegs,
+        TecE,
+        ADD
+    );
+
+  FF_ENT_DADO: FLIP_FLOP_D
+    GENERIC MAP(
+        8
+    );
+    PORT MAP(
+        CLKR,
+        ClearRegs,
+        TecD,
+        DATA_IN
+    );
+
+  FF_ENT_DADO: FLIP_FLOP_D
+    GENERIC MAP(
+        1
+    );
+    PORT MAP(
+        CLKR,
+        ClearRegs,
+        WrEn,
+        WR
+    );
+
+
+  RAM_16: RAM_32x8
+      PORT(
+          ADD,
+          DATA_IN,
+          WR,
+          CLKM,
+          DATA_OUT
+      );
+  
 
 	-- State's table
-	PROCESS(X, Y_D)
-	BEGIN
-		CASE Y_D IS
-			-- Begining Node
-			WHEN A=>
-				IF (X = '0') THEN Y_Q <= B;
-				ELSE Y_Q <= F;
-				END IF;
+	PROCESS(CLKR, CLKM, CLKZ) BEGIN
+    
 
-			-- 0 Branch
-			WHEN B=>
-				IF (X = '0') THEN Y_Q <= C;
-				ELSE Y_Q <= F;
-				END IF;
-			WHEN C=>
-				IF (X = '0') THEN Y_Q <= D;
-				ELSE Y_Q <= F;
-				END IF;
-			WHEN D=>
-				IF (X = '0') THEN Y_Q <= E;
-				ELSE Y_Q <= F;
-				END IF;
-			WHEN E=>
-				IF (X = '0') THEN Y_Q <= E;
-				ELSE Y_Q <= F;
-				END IF;
 
-			-- 1 Branch
-			WHEN F=>
-				IF (X = '1') THEN Y_Q <= G;
-				ELSE Y_Q <= B;
-				END IF;
-			WHEN G=>
-				IF (X = '1') THEN Y_Q <= H;
-				ELSE Y_Q <= B;
-				END IF;
-			WHEN H=>
-				IF (X = '1') THEN Y_Q <= I;
-				ELSE Y_Q <= B;
-				END IF;
-			WHEN I=>
-				IF (X = '1') THEN Y_Q <= I;
-				ELSE Y_Q <= B;
-				END IF;
 
-		END CASE;
-	END PROCESS;
+  -- Output definition
+  Y_P_Vd <= '1' WHEN (Y_D = Vd_Vm) ELSE '0';
+  Y_P_Am <= '1' WHEN (Y_D = Am_Vm) ELSE '0';
+  Y_P_Vm <= '1' WHEN (Y_D = Vm_Vd OR Y_D = Vm_Am) ELSE '0';
+  Y_S_Vd <= '1' WHEN (Y_D = Vm_Vd) ELSE '0';
+  Y_S_Am <= '1' WHEN (Y_D = Vm_Am) ELSE '0';
+  Y_S_Vm <= '1' WHEN (Y_D = Vd_Vm OR Y_D = Am_Vm) ELSE '0';
 
-	-- How FFs will behave
-	PROCESS(CLK, RST)
-	BEGIN
-		IF(RST = '1') THEN
-			Y_D <= A;
-		ELSIF (rising_edge(CLK)) THEN
-			Y_D <= Y_Q;
-		END IF;
-	END PROCESS;
-
-	-- Output definition
-	Y <= '1' WHEN (Y_D = E OR Y_D = I) ELSE '0';
-
-END ARCHITECTURE ARCH_PARTE02;
+END ARCHITECTURE ARCH_PARTE01;
